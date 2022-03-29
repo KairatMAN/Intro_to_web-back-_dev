c0879f313173fd75ce52d8f4cf0f366e79e11ad1
Myrza <kkubanychbekuulu99@gmail.com>
2022-03-29 18:19:37 +0600

    *

--- quotes_scrapy.py
+++ quotes_scrapy.py
@@ -3,16 +3,21 @@ import requests
 
 url = 'https://quotes.toscrape.com/'
 
-data = requests.get(url).text
 
+
+data = requests.get(url).text
 html_data = BeautifulSoup(data,'html.parser')
 
+
 quotes = html_data.find_all(class_='quote')
-schet = 0
+counter = 0
+
 for quote in quotes:
-    result = quote.find(class_='text')
+    text = quote.find(class_='text')
     author = quote.find(class_="author")
-    schet +=1
-    print(schet,")",sep="")
-    print(result.get_text(),author.get_text(),sep="\tby  ",end="\n")
+    tags = quote.find(class_="keywords")
+    counter +=1
+    print(counter,")",sep="")
+    print(text.get_text(),author.get_text(),sep="\tby  ",end="\n")
+    print("Tags:",tags['content'])
 # print(html_data.get_text())
